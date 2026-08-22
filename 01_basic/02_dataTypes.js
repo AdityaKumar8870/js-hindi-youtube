f5aaeaecd51adc463aba6553f0afcbaab287358a
Aditya Kumar <ap0443785@gmail.com>
2026-08-22 10:47:54 +0000

    data type

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) we are using nodejs,not browser
+
+let name ="Aditya"
+let age=22
+let isLoggedIn=false
+let state;
+
+// these are a primitive data types
+//number=>2 to power 53
+//bigint
+//String=>""
+// boolean => true/false
+//null=stndalone value
+//symbol=>unique
+
+
+//object
+
+console.log(typeof name);
+console.log(typeof null);
+
+
+
+
