057da0cd624d75e1a825d24bc655950cef615d9b
Aditya Kumar <ap0443785@gmail.com>
2026-08-22 05:45:43 +0000

    container config

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail="ap0443785@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+
+// accountId=12121212
+
+// console.log(accountId);
+
+accountEmail="sanket@gmail"
+accountPassword="2121212"
+accountCity="benguluru"
+
+
+/*
+
+prefer not use var
+because of issue in block and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
