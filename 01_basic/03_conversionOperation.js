dd12041d790ead031305255e5ad0598475cfd0f7
Aditya Kumar <ap0443785@gmail.com>
2026-08-23 03:51:19 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,78 @@
+let score = 33
+//console.log(typeof score);
+
+let StringScore = String(score);
+//console.log(StringScore);
+//console.log(typeof StringScore);
+
+
+let name ="Aditya"
+//console.log(typeof name);
+let ConvertString=Number(name);
+
+//console.log(typeof ConvertString);
+
+//"33"=>33
+//"33sabc"=>NaN not a number 
+// true =>1 , false = > 0
+
+let value=true
+ //console.log(typeof value);
+
+ let valueNumber=Number(value);
+ //console.log(typeof valueNumber);
+ //console.log(valueNumber);
+
+ //let isLoggin=1
+ // let isLoggin=0
+ //let isLoggin=""
+
+//  let isLoggin="Aditya"
+//  console.log(typeof isLoggin);
+ 
+//  let convertLogin=Boolean(isLoggin);
+//  console.log(typeof convertLogin);
+//  console.log(convertLogin);
+ 
+ 
+ // 1=> true; 0 => false
+ //"" => false
+ //"Aditya" => true
+
+
+
+ //***********************Operations*******************************
+
+let valuee =3
+let negValue=-valuee
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(3-3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%4);
+
+let str1 ="Hello";
+let str2=" Aditya";
+let str3 = str1+str2;
+console.log(str3);
+
+
+console.log(1+"2");
+console.log(1+"2"+"2");
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+false);
+
+
+
+ 
+
+
+
+
