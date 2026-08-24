a9175bb8d23ede94243cbe98e596c159e69d654a
Aditya Kumar <ap0443785@gmail.com>
2026-08-24 14:04:50 +0000

    String

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,100 @@
+let name = "Aditya";
+let str1="hello";
+let str2="world";
+
+// let str3 =str1+str2;
+
+// this is a new way to concatenation 
+// console.log(`My name is Aditya ${name} and i want to say ${str1} or ${str2}`);
+
+// Now i am declaring String in object form and  apply some method to it 
+
+let myName = new String("Aditya Pandey");
+//console.log(myName);
+
+// anchor() method is used to create a link.
+// console.log(myName.anchor("link"));
+
+
+// at() method is used to get a charactewr at specific index in a string but it also support a negative index
+// console.log(myName.at(-1));
+
+
+//big() method is use to display a text in larger size 
+
+// console.log(myName.big());
+
+
+// bold() method is used to diplay text in bold size 
+// console.log(myName.bold());
+
+// charAt() method is used to get character at specific index
+
+
+// console.log(myName.charAt(5));
+
+
+// charCodeAt() is used to get a character at a specified index
+// and return its ASCII value.
+// console.log(myName.charCodeAt(4));
+
+
+// codePointAt() method is same as the charCodeAt().
+// console.log(myName.codePointAt(4));
+
+
+// concat() method is used to concatenate (join) two or more strings.
+// If you want to add/join another string at the end of a string, you can use concat()
+// console.log(myName.concat("kumar"));
+
+
+// endsWith() method is used to check whether a string ends with
+// a specified character or string. It returns true or false.
+// console.log(myName.endsWith('e'));
+
+
+
+// includes() method is used to check whether a specified character
+// or string is present in a string or not. It returns true or false.
+// console.log(myName.includes('Aditya'));
+
+// trim() this method is used to remove extra space from starting and ending postion
+// console.log(myName.trim());
+
+
+// slice() method is used to extract a portion of a string
+// based on the specified start and end indexes or end indexes is excluided.
+// console.log(myName.slice(0,4));
+
+
+
+// substring() method is similar to slice().
+// Both are used to extract a portion of a string,
+// but substring() does not support negative indexes.
+// console.log(myName.substring(0,5));
+
+
+
+// indexOf() method is used to find the index (position)
+// of a specified character or string.
+// console.log(myName.indexOf('i'));
+
+
+// split("")   // split into characters
+// split(" ")  // split wherever a space occurs
+// console.log(myName.split(" "));
+
+
+// replace() is used to replace a specified value in a string. replace only the first matching occurence
+
+//  console.log(myName.replace("Aditya","Sanket"));
+
+
+// replaceAll() is used to replace all occurrences of a specified value in a string.
+
+// console.log(myName.replaceAll("Aditya","Sanket"));
+
+
+
+
+
