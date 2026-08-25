4b13fb5c9ad1346395eafa1de19aa01f502ea47b
Aditya Kumar <ap0443785@gmail.com>
2026-08-25 02:36:56 +0000

    Math and Num

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(100000000);
+// console.log(balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+// console.log(balance.toLocaleString("en-IN"));
+
+//++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.91324));
+// console.log(Math.ceil(5.231));
+// console.log(Math.floor(5.91));
+
+
+
+//Most Important
+
+// console.log(Math.random());
+// console.log(Math.random()*10+1);
+
+
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))  +min);
+
+
+
+
+
+
