7e2a7580010bccf5d8c326d2daedc1a0e3115fe0
Aditya Kumar <ap0443785@gmail.com>
2026-08-25 03:44:34 +0000

    date and time

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,26 @@
+//Date
+
+let myDate=new Date();
+ console.log(myDate.toString());
+ //console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026 ,7,25);
+// let myCreatedDate = new Date(2026 ,7,25,8,58);
+
+// let myCreatedDate = new Date("2026-08-25");
+let myCreatedDate = new Date("08-25-2026");
+
+
+
+//console.log(myCreatedDate.toLocaleString());
+
+// toString()             → Date + Time as a full string
+// toLocaleString()       → Date + Time in local format
+// toLocaleDateString()   → Only Date in local format
+
+
+
+
