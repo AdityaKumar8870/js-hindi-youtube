b88149b627c37862b116c75013790c163057acda
Aditya Kumar <ap0443785@gmail.com>
2026-08-26 03:33:34 +0000

    arrys basic 1

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,60 @@
+// array
+const number = [0,1,2,3,4,5];
+const multiVAr=[0,1,2,3,4,"Aditya",true];
+// 
+
+
+const Myarr= new Array(0,1,2,3,4,5);
+// console.log(Myarr);
+
+// console.log(Myarr.length);
+// console.log(Myarr.indexOf(9));
+// console.log(Myarr.includes(3));
+
+
+
+//push method is used to add an element at last posistion in array.
+// Myarr.push(6);
+// console.log(Myarr);
+
+
+// // pop() method is use to remove an element at last position in array.
+// Myarr.pop();
+// console.log(Myarr);
+
+
+// unshift method is used to add an element at starting position in array
+//  Myarr.unshift(8)
+// console.log(Myarr)
+
+//shift method is used to remove an element at starting position in array.
+
+// Myarr.shift();
+// console.log(Myarr);
+
+
+
+// const newArr=Myarr.join();
+// console.log(Myarr);
+// console.log(newArr);
+
+
+//slice , splice 
+
+console.log("A " , Myarr);
+const mn1 = Myarr.slice(1,3);
+console.log(mn1);
+
+console.log("B ",Myarr);
+
+const mn2 = Myarr.splice(1,3);
+console.log("c ",Myarr);
+console.log(mn2);
+
+
+
+
+
+
+
+
