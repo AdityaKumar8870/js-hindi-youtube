11d7f1912d31afacc5126b5f5761a8378d11e0f2
Aditya Kumar <ap0443785@gmail.com>
2026-09-07 11:32:57 +0000

    scopes

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,55 @@
+let a = 20 ;
+const b = 30 ;
+var c = 340 ;
+
+if(true){
+    let a = 10;
+    const b =40;
+    var c =30;
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one (){
+//     const username = "Aditya";
+
+//     function two(){
+//         console.log(username);
+//         const website="youtube";
+
+//     }
+//    // console.log(website);
+//    two();
+
+    
+// }
+// one()
+
+
+// if ( true){
+//     const username = "aditya";
+
+//     if(username === "aditya"){
+//         const website = "youtube";
+//         console .log(username+website);
+//     }
+//    // console.log(website);
+// }
+// //console.log(username);
+
+addOne();
+function addOne (num){
+    return num +1;
+}
+addOne();
+
+
+const addTwo=function(num){
+    return num+2;
+}
+
+addTwo();
+
+
